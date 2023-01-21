41f6041d6cdade89c7b60a5ec9abd86c9b06662a
GausPeerzade <gauspeerzade@gmail.com>
2023-01-21 23:57:47 +0530

    Changed fot multiple student registration

--- StudentDataBase/StudentDataBase.java
+++ StudentDataBase/StudentDataBase.java
@@ -1,13 +1,20 @@
 package StudentDataBase;
+import java.util.*;
+
 
 public class StudentDataBase {
     public static void main(String[] args) {
-    
-    Student s1 = new Student();
-    
+        
+        Scanner sc = new Scanner(System.in);
+
         // Ask how many students we want to Add
-
-
-    // Create n numbers of students
+   System.out.print("Enter How Many Students You want to add :- ");
+   
+   int number = sc.nextInt();
+    
+   // Create n numbers of students
+   for(int i = 0 ;i < number ; i++){
+       Student s1 = new Student();
+   }
     }
 }
